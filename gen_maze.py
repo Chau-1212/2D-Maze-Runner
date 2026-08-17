501430af57fadc41bc6784fd85342d5ac51fe2d0
Chau <winglokchau2008@outlook.com>
2026-08-17 22:27:44 +0100

    continue to work in gen_maze.py. fnish create_grid(), WIP dfs()

--- gen_maze.py
+++ gen_maze.py
@@ -1,13 +1,17 @@
+import random
+
+from arcade.color import NEW_YORK_PINK
+
 class Maze:
     def __init__(self,room,maze,hallway):
-        self.room = room
+        self.room = room #room size
         self.size = maze
         self.hallway = hallway
         self.stride = room + hallway
 
-        self.visited = [ #new 2d list for maze visited
-            [False] * self.size #it just set the room cood to false
-            for _ in range(self.size)
+        self.visited = [ #new 2d list for maze visited. is a ROOM 2d list
+            [False] * self.size #it just set the room cood to false. Thing we put inside the list
+            for _ in range(self.size) #using _ becuase we don care about the number. we create self.size amount of row
         ]
 
 
@@ -26,8 +30,47 @@ class Maze:
             for column in range(self.size):
 
                 x = column * self.stride + 1 #because there is a outter wall
-                y = column * self.stride + 1
+                y = row * self.stride + 1
 
                 for room_y in range(self.room):
                     for room_x in range(self.room):
                         self.maze[y+room_y][x + room_x] = 1 #it change along the maze with x,y
+
+    def dfs(self,row,column):
+        self.visited[row][column] = True
+
+        directions= [
+            (-1,0), #up
+            (1,0), #down
+            (0,-1), #left
+            (0,1) #right
+        ]
+
+        random.shuffle(directions)
+
+        for dir_row, dir_column in directions:
+            new_row = row + dir_row #New column coord for room
+            new_column = column + dir_column #New row coord for room
+
+            if (
+                0 <= new_row < self.size#that is how many room are there
+                and
+                0 <= new_column < self.size
+                and not
+                self.visited[new_row][new_column]
+            ):
+                x = column * self.stride + 1
+                y = row * self.stride + 1
+                # this point to the top left tile of the room
+
+                new_x = new_column * self.stride + 1
+                new_y = new_row * self.stride + 1
+                #the new top left tile of the new room from the directoin
+
+                if dir_row != 0: #if row change
+                    #up or down
+                    passage_x = x + self.room // 2 #make sure it is in the middle of the room
+                    passage_y = y + self.room
+                else: #left or right
+                    passage_x = x + self.room
+                    passage_y = y + self.room // 2
