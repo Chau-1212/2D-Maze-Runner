6abcc43a8c5531ba414618f054990ccea7ef11da
Chau <winglokchau2008@outlook.com>
2026-08-17 16:01:00 +0100

    add gitignore

--- furance.py
+++ furance.py
@@ -2,10 +2,10 @@ import arcade
 
 class furance:
     def __init__(self):
-        self.fuel = 0
+        self.fuel = 0 #how much object "fuel"
         self.cooking = False
         self.content = 0
-        self.cook_timer = 0
+        self.cook_timer = 0#how much cooking time left
         self.product = 0
 
     def add_coal(self): #we have the minus the coal from the player inveotry
@@ -20,12 +20,12 @@ class furance:
             self.cooking = True
 
             if self.cooking:
-                self.cook_timer += delta_time
+                self.cook_timer += delta_time #So it stay the same across different fps
 
                 if self.cook_timer >= 20:
                     self.cook_timer = 0
                     self.content -= 1
-                    self.product += 1
+                    self.product += 1 #product will be cooked meat
                     self.cooking = False #process are saved becuase the cook timer isn't reset to zero
 
         else:
