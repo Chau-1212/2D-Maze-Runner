1adf8d5cf434a43d7e14ac1db29a48b76a8f2e9f
Chau <winglokchau2008@outlook.com>
2026-08-18 16:40:30 +0100

    added time_state.py Finished

---
+++ time_state.py
@@ -0,0 +1,26 @@
+import random
+
+class time_state:
+    def __init__(self):
+        self.timer = 0
+        self.state = "day"
+
+    def update(self,delta_time):
+        self.timer += delta_time
+
+        if self.state == "day":
+            if self.timer >= 180:
+                self.state = "night"
+                self.timer = 0
+
+        elif self.state == "night":
+            if self.timer >= 120:
+                if random.randint(0,100) <= 20:
+                    self.state = "blood moon"
+                else:
+                    self.timer = 0
+
+        elif self.state == "blood moon":
+            if self.timer >= 30:
+                self.state = "day"
+                self.timer = 0
