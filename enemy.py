abecbfe47baf72dc89718928f01bb8364c365de0
Chau <winglokchau2008@outlook.com>
2026-08-24 19:05:51 +0100

    completed BFS logic, didn't plug it inside chasing yet

--- enemy.py
+++ enemy.py
@@ -1,5 +1,7 @@
 import random
 
+from pyglet.window.key import _0
+from collections import deque
 from maze import TILE_SIZE
 from physic import is_walkable
 import math
@@ -49,8 +51,8 @@ class enemy:
 
         return(room_x,room_y)
 
-    def get_available_direction(self,maze,stride,room_size):
-        room_x,room_y = self.get_room(stride)
+    def get_available_direction(self,room,maze,stride,room_size):
+        room_x,room_y = room
         directions=[]
 
         x = room_x * stride + 1 #Room x is the room number in x, thes self contain current room coord
@@ -74,6 +76,52 @@ class enemy:
 
         return directions
 
+    def bfs(self, start, target, maze, stride, room_size):
+        #It guarantte the shortest route
+        queue = deque([start]) #It is more effeicnet than a normal list for removing thing in both end unlike list you have to move everything if you move the first item
+        visited = {start} #Is a python data structrue call set that is faster look up
+        previous = {start: None} # this create a dictionary. Start: previous value
+
+        while queue: #when it is not empty
+            current = queue.popleft() #take the enxt room from queue
+
+            if current == target: #have we find the player yet
+                break
+
+            directions = self.get_available_direction(
+                current,
+                maze,
+                stride,
+                room_size
+            ) #search which neighbouring room can we enter
+
+            for directoin in directions:
+                next_room = (
+                    current[0] + directions[0], #x
+                    current[1] + directions[1] #y
+                ) # calculate the neighbouring room coord
+
+                #because we are using set we don have to use for loop
+                if next_room not in visited:
+                    visited.add(next_room) # make it visited
+                    previous[next_room] = current #remember where we come from
+                    queue.append(next_room)
+
+        if target not in previous:
+            return [] # There is no path to the player
+
+        path = []
+        current = target
+
+        while current != start: # keep going back ward
+            path.append(current)
+            current = previous[current] # loop through the dcionary key
+
+        path.reverse() #from target -> self to sekf -> target
+
+        return path
+
+
     def patrol(self, delta_time, stride, maze, room_size):
 
         current_room = self.get_room(stride)
@@ -83,6 +131,7 @@ class enemy:
             self.room = current_room
 
             directions = self.get_available_direction(
+                current_room,
                 maze,
                 stride,
                 room_size
@@ -102,19 +151,47 @@ class enemy:
         #It will only move if it is walkable
 
 
-
-
-
-
-    def chasing(self,player,stride):
+    def chasing(self,player,stride,current_time_state, delta_time, maze):
         #Player is the player object
         enemy_room = self.get_room(stride)
         player_room = player.get_room(stride)
 
-        #now chekc are they in sam room
+        #Determine vision
+        if current_time_state == "night":
+            vision = self.vision
+
+        elif current_time_state == "blood moon":
+            vision = self.buffed_vision
+
+
+
+        #Same room -> diret chase
         if enemy_room == player_room:
             # directly approach it
-            pass
+
+            #Find the difference between themn first
+            dx = player.x - self.x
+            dy = player.y - self.y
+
+            # A^2 + B^2 = C^2
+            distance = math.sqrt(dx ** 2 + dy ** 2)
+
+            if distance == 0:
+                return
+
+            #normalise the factor so it don change base on distance
+            dx /= distance
+            dy /= distance
+
+
+            new_x= self.x + dx * self.speed * delta_time
+            new_y = self.y + dy * self.speed * delta_time
+
+            if is_walkable(new_x, new_y,maze):
+                self.x = new_x
+                self.y = new_y
+
+
         else:
             # not in the same room, now check if it is within vision range
             room_distance = math.sqrt(
@@ -123,4 +200,8 @@ class enemy:
                 (enemy_room[1] - player_room[1]) ** 2
             )
 
-            if
+            if room_distance > vision:
+                self.state = "patrol"
+                return #end the function becuase it is no longer in spider vision
+
+            else: #player in vision BFS mechanic
